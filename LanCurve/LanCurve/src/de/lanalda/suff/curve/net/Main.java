675f033584bb794e8ecb19a8d77574b2b77e707f
Tobias Bechtold <tobi.bechtold@gmail.com>
2015-12-28 17:09:08 +0100

    removed unnecessary classes

--- LanCurve/LanCurve/src/de/lanalda/suff/curve/net/Main.java
+++ LanCurve/LanCurve/src/de/lanalda/suff/curve/net/Main.java
@@ -2,15 +2,17 @@ package de.lanalda.suff.curve.net;
 import java.net.URI;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 
 import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.server.ResourceConfig;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		ResourceConfig rc = new ResourceConfig().packages("de.lanalda.suff.curve.net");
-		HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create( "http://localhost:8080/" ), rc);
+		String[] s = new String[1];
+		s[0] = "de.lanalda.suff.curve.net";
+		ResourceConfig rc = new ResourceConfig().packages(s);
+		HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create("http://localhost:8080/"), rc);
 	}
 
 }
